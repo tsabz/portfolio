7fa627139a57b3a55725864f9c270f2a25b3711d
Tonia Saba <36241267+tsabz@users.noreply.github.com>
2020-08-24 15:42:30 -0400

    created landing page

--- sabafolio/src/App.js
+++ sabafolio/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
 // import logo from './logo.svg';
 import './App.css';
+import Landing from './Landing.js';
 import Projects from './Projects.js';
 import Articles from './Articles.js';
 import About from './About.js';
@@ -12,13 +13,15 @@ function App() {
 
       <div className="App">
 
-          <Route exact path= '/' component={Projects} />
+          <Route exact path= '/' component={Landing} />
+          <Route exact path= '/projects' component={Projects} />
           <Route exact path= '/articles' component={Articles} />
           <Route exact path= '/about' component={About} />
 
           <div className="navigation">
               <div className="navigation-sub">
-                  <Link to="/" className="item">Projects</Link>
+                  <Link to="/" className="item">Home</Link>
+                  <Link to="/projects" className="item">Projects</Link>
                   <Link to="/articles" className="item">Articles</Link>
                   <Link to="/about" className="item">About</Link>
               </div>
