01ab02e05db63b0f97be15930928f003053f84fb
Tonia Saba <36241267+tsabz@users.noreply.github.com>
2020-08-25 15:37:07 -0400

    new set up and nav bar made with bootstrap/react

--- sabafolio/src/App.js
+++ sabafolio/src/App.js
@@ -1,33 +1,59 @@
 import React from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
-// import logo from './logo.svg';
-import './App.scss';
-import Landing from './Landing.js';
-import Projects from './Projects.js';
-import Articles from './Articles.js';
-import About from './About.js';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './App.css';
+import NavbarBrand from 'react-bootstrap/esm/NavbarBrand';
 
-function App() {
-  return (
-    <BrowserRouter>
 
-      <div className="App">
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Tonia Saba',
+      headerLinks: [
+        { title: 'home', path: '/'},
+        { title: 'projects', path: '/projects'},
+        { title: 'resume', path: '/resume'}
+      ],
+      home: {
+        title: 'Blah',
+        subTitle: 'blah blah',
+        text: 'blah blah blah'
+      },
+      projects : {
+        title: 'projects'
+      },
+      resume: {
+        title: 'resume'
+      }
+    }
+  }
 
-      <div className="navigation">
-              <div className="navigation-sub">
-                  <Link to="/" className="item">Home</Link>
-                  <Link to="/projects" className="item">Projects</Link>
-                  <Link to="/articles" className="item">Articles</Link>
-                  <Link to="/about" className="item">About</Link>
-              </div>
-          <Route exact path= '/' component={Landing} />
-          <Route exact path= '/projects' component={Projects} />
-          <Route exact path= '/articles' component={Articles} />
-          <Route exact path= '/about' component={About} />
-          </div>
-      </div>
-    </BrowserRouter>
-  )
-}
+  render() {
+    return (
+      <Router>
+          <Container className="p-0" fluid={true}>
+            
+            <Navbar className="border-bottom">
+              <Navbar.Brand>Tonia Saba</Navbar.Brand>
+              <Navbar.Toggle aria-controls="navbar-toggle" />
+              <Navbar.Collapse id="navbar-toggle">
+                <Nav>
+                  <Link className="nav-link" to="/">Home</Link>
+                  <Link className="nav-link" to="/projects">Projects</Link>
+                  <Link className="nav-link" to="/resume">Resume</Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+            
+            </Container>
+        
+        </Router>
+      );
+    }
+  }
+ 
 
 export default App;
