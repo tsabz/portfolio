2a2c185059b2a3a889fbd302a1c8e7091f641d57
Tonia Saba <36241267+tsabz@users.noreply.github.com>
2020-08-26 18:35:08 -0400

    added git hub button to home page with rainbow effect

--- sabafolio/src/components/jumbo.js
+++ sabafolio/src/components/jumbo.js
@@ -3,6 +3,7 @@ import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
 import Tonia from '../assests/images/toniasaba.JPG'
 
 
@@ -15,9 +16,10 @@ function Jumbo(props) {
                 <Row className="justify-content-center py-5">
                     <img className="g-card-homeimage" src={Tonia} alt={Tonia}/>
                     <Col md={8} sm={12}>
-                        { props.title && <h1 className="display-1 front-weight-bolder">{props.title}</h1> }
+                        { props.title && <h1 className="display-3 front-weight-bolder">{props.title}</h1> }
                         { props.subTitle && <h3 className="display-4 front-weight-light">{props.subTitle}</h3> }
                         { props.text && <h3 className="lead front-weight-light">{props.text}</h3> }
+                        <Button className="button" href="https://github.com/tsabz" target="_blank" rel="noopener noreferrer" variant="light" size="lg" block>Github</Button>
                     </Col>
                 </Row>
             </Container>
